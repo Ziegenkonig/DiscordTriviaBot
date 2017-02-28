99c32773582f398166ebf0c1fa49ce7afba8d45b
Alex Ziegenhorn <Alex Ziegenhorn>
2017-02-27 23:18:43 -0600

    Added greet command, started work on command logic

--- DiscordTriviaBot/TriviaBot.cs
+++ DiscordTriviaBot/TriviaBot.cs
@@ -15,11 +15,17 @@ namespace DiscordTriviaBot
         private DiscordClient client;
         private CommandService commands;
 
+        //Constructor - also includes all command instantiation
         public TriviaBot()
         {
+            //Creating DiscordClient object
             client = new DiscordClient();
+
+            //Assigning Callbacks
             client.JoinedServer += JoinedServerCallback;
             client.MessageReceived += MessageRecievedCallback;
+
+            //Telling the program that client is using a CommandService
             client.UsingCommands(command =>
             {
                 command.PrefixChar = '!';
@@ -28,9 +34,17 @@ namespace DiscordTriviaBot
                 command.ExecuteHandler = CommandExecutedCallback;
             });
 
+            //Hooking up our commands object to our client's CommandService 
             commands = client.GetService<CommandService>();
+
+            //!trivia - Display all commands and their descriptions
             commands.CreateCommand("trivia")
                 .Description("Lists all commands provided by Trivia Bot.")
+                .Do(async (e) => { await CommandCreationCallback(e); });
+            //!greet - Greets the user, because this bot is passive aggressive not outright rude
+            commands.CreateCommand("greet")
+                .Alias("gr", "hello", "hi")
+                .Description("Greets the plebian who calls for it")
                 .Do(async (e) => { await CommandCreationCallback(e); });
         }
 
@@ -47,27 +61,48 @@ namespace DiscordTriviaBot
         /* Callbacks                        */
         /* ================================ */
 
+        //Called when each command is created in the constructor
         private async Task CommandCreationCallback(CommandEventArgs e)
         {
+            await e.Server.DefaultChannel.SendMessage("");
             await e.Server.DefaultChannel.SendMessage("Description: " + e.Command.Description);   
         }
-
+        //Called when a command fails to execute
         private async void CommandErroredCallback(object s, CommandErrorEventArgs e)
         {
-            await e.Server.DefaultChannel.SendMessage("!" + e.Command.Text + "failed execution with error.");
+            if (!e.Message.IsAuthor)
+                await e.Server.DefaultChannel.SendMessage("!" + e.Command.Text + " failed execution with error.");
         }
-
+        //Called whenever a command is executed by a user
         private async void CommandExecutedCallback(object s, CommandEventArgs e)
         {
-            await e.Server.DefaultChannel.SendMessage("!" + e.Command.Text + " was executed.");
+            //Switch statement that handles all of the logic for our commands
+            switch (e.Command.Text) {
+                case "greet": //!greet
+                    await e.Server.DefaultChannel.SendMessage($"{e.Command.Text}");
+                    await e.Server.DefaultChannel.SendMessage($"Hello {e.User.Name}! Riddle me this!");
+                    break;
+                case "trivia": //!trivia
+                    String helptext = "```";
+                    foreach (Command command in commands.AllCommands)
+                        helptext = helptext + $"!{command.Text} - {command.Description}\n";
+                    helptext = helptext + "```";
+                    await e.Server.DefaultChannel.SendMessage(helptext);
+                    break;
+                case "help": //!help
+                    break;
+                default: //this should probably never be run
+                    await e.Server.DefaultChannel.SendMessage("!" + e.Command.Text + " was executed.");
+                    break;
+            }
         }
 
         private async void MessageRecievedCallback(object s, MessageEventArgs e)
         {
             // Bot can recieve its own messages, 
             // therefore we only want to check non-bot messages.
-            if(!e.Message.IsAuthor)
-                await e.Server.DefaultChannel.SendMessage("Testing");
+            //if(!e.Message.IsAuthor)
+            //    await e.Server.DefaultChannel.SendMessage("Testing");
         }
 
         private async void JoinedServerCallback(object s, ServerEventArgs e)
